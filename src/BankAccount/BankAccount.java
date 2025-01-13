f0ba55eaa94d8e798e23b9d1b89f77018afa1efa
hunanyam_wlhs <hunanyam@wlhs.wlwv.k12.or.us>
2025-01-13 15:02:10 -0800

    created, testing

--- src/BankAccount/BankAccount.java
+++ src/BankAccount/BankAccount.java
@@ -1,59 +1,55 @@
 package BankAccount;
-import java.util.Random;
+
 /**
-* Operations: deposit money, withdraw money, get current balance
-* Instance variables (what makes a bank account, it's properties):
-* account number, balance, interest rate, bank, owner
-*/
+ * Operations: deposit money, withdraw money, get current balance
+ * Instance variables (what makes a bank account, it's properties):
+ * account number, balance, interest rate, bank, owner
+ */
 
-    public class BankAccount {
+public class BankAccount {
 
-        private int accountNumber;
-        private double balance;
-        private double interestRate;
-        private String bank;
-        private String owner;
-        Random rand = new Random();
-        //constructors must have same name as class
+    private String accountNumber;
+    private double balance;
+    private double interestRate;
+    private String bank;
+    private String owner;
 
-        public BankAccount(){
-            balance = 0;
-            accountNumber = rand.nextInt(100000000);
-        }
-
-        //overloaded constructor
-        public BankAccount(double initialBalance){
-            balance = initialBalance;
-        }
-
-        public BankAccount(String name, double initialBalance)
-        {
-
-        }
-        //setters (mutators)
-print
-        /**
-         * Deposits a specified amount of money into the
-         * account
-         * @param amount amount to deposit
-         */
-        public void deposit(double amount){
-            balance += amount;
-        }
-
-        public void withdraw(double amount){
-            balance -= amount;
-        }
-
-        //getter (accessor)
-        public double getBalance(){
-            return balance;
-        }
-
-        @Override
-        public String toString(){
-            return String.format("Balance: $%.2f",balance);
-        }
+    //constructors must have same name as class
+    public BankAccount(){
+        balance = 0;
     }
 
+    //overloaded constructor
+    public BankAccount(double initialBalance){
+        balance = initialBalance;
+    }
 
+    public BankAccount(String name, double initialBalance){
+        balance = initialBalance;
+    }
+
+    //setters (mutators)
+
+    /**
+     * Deposits a specified amount of money into the
+     * account
+     * @param amount amount to deposit
+     */
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
+    //getter (accessor)
+    public double getBalance(){
+        return balance;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Balance: $%.2f",balance);
+    }
+}
