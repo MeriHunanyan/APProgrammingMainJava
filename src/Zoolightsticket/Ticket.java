109319cb0195c2852416ca8fa872ae6d6e7ad9d4
hunanyam_wlhs <hunanyam@wlhs.wlwv.k12.or.us>
2025-02-07 14:54:29 -0800

    Got the first parts of Zoo lights

---
+++ src/Zoolightsticket/Ticket.java
@@ -0,0 +1,25 @@
+package Zoolightsticket;
+public class Ticket {
+
+    private final int ID;
+    private final String userName;
+    private final int age;
+    private final double cost;
+    private final boolean canDrink;
+    private final boolean canRideTrain;
+
+    public Ticket(int ID, String userName, int age, double cost, boolean canDrink, boolean canRideTrain){
+        this.ID = ID;
+        this.userName = userName;
+        this.age = age;
+        this.cost = cost;
+        this.canDrink = canDrink;
+        this.canRideTrain = canRideTrain;
+    }
+    public int getTicketID(){
+        return ID;
+    }
+    public void printTicket(){
+        System.out.printf("%nID: %d%nNAME: %s%nAGE: %d%nCOST: %.2f%nCAN ACCESS ALCOHOL AREA: %s%nCAN RIDE TRAIN: %s%n", ID, userName, age, cost, canDrink, canRideTrain);
+    }
+}
